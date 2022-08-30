ea1317fdcd4c11063d571fd3df15b0ff08949dbe
Adam Fortuna <adam@fortuna.name>
2022-08-29 18:57:14 -0600

    Fixed next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-  poweredByHeader: false,
-  generateEtags: false,
-  /**
-   * Environment Variables.
-   */
-  publicRuntimeConfig: {
-    APP_NAME: process.env.APP_NAME,
-  },
-  serverRuntimeConfig: {},
   images: {
     domains: ['res.cloudinary.com'],
   },
