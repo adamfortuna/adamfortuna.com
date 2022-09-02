c0e21797849ed732c4900f5ba48df59ad9867870
Adam Fortuna <adam@fortuna.name>
2022-09-01 19:55:59 -0600

    Added animation for image loading

--- src/components/layout/Image.tsx
+++ src/components/layout/Image.tsx
@@ -1,13 +1,23 @@
 /* eslint-disable react/jsx-props-no-spreading, jsx-a11y/anchor-has-content */
+import { useState } from 'react'
 import NextImage, { ImageProps } from 'next/image'
 import { useImagePlaceholder } from '@/hooks/useImagePlaceholder'
 
 export const Image = (props: ImageProps) => {
   const { src } = props
   const { blurDataURL } = useImagePlaceholder(src as string)
+  const [loaded, setLoaded] = useState(false)
 
   if (blurDataURL) {
-    return <NextImage blurDataURL={blurDataURL} placeholder="blur" {...props} />
+    return (
+      <NextImage
+        {...props}
+        blurDataURL={blurDataURL}
+        placeholder="blur"
+        className={loaded ? 'unblur' : ''}
+        onLoadingComplete={() => setLoaded(true)}
+      />
+    )
   }
   return <NextImage {...props} />
 }
