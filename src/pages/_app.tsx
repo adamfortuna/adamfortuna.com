fca1be6cdd65d7a0abcdedbe7121ef9d45f2392f
Adam Fortuna <adam@fortuna.name>
2022-11-08 16:38:26 -0700

    Removed plaiceholder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,14 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { useMemo } from 'react'
 import { AppProps } from 'next/app'
 
 import '@/styles/tailwind.css'
-import { ImageContext } from '@/hooks/useImagePlaceholder'
 import { Layout } from '@/components/layout/Layout'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const imageContext = useMemo(() => {
-    return pageProps.imagePlaceholders || new Map()
-  }, [pageProps])
-
   return (
-    <ImageContext.Provider value={imageContext}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ImageContext.Provider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   )
 }
 
