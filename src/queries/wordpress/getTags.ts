4ff49a5334a7012fd6290930a2c2ce2ce7148656
Adam Fortuna <adam@fortuna.name>
2023-01-10 17:58:30 -0700

    Fixed tags page

--- src/queries/wordpress/getTags.ts
+++ src/queries/wordpress/getTags.ts
@@ -1,5 +1,7 @@
-import WordpressClient, { parseTags } from '@/lib/wordpressClient'
+import { getClientForProject, parseTags } from '@/lib/wordpressClient'
 import { gql } from '@apollo/client'
+import { Tag, WordpressClientIdentifier } from '@/types'
+import { flatten } from 'lodash'
 
 export const findWordPressTags = gql`
   query GetTags {
@@ -13,7 +15,35 @@ export const findWordPressTags = gql`
   }
 `
 
+export const getTagsByProject = async (project: WordpressClientIdentifier) => {
+  return getClientForProject(project)
+    .query({ query: findWordPressTags })
+    .then((result) => parseTags(result.data.tags.nodes))
+}
+
 export const getTags = async () => {
-  const result = await WordpressClient.query({ query: findWordPressTags })
-  return result.data.tags ? parseTags(result.data.tags.nodes) : []
+  const projects: WordpressClientIdentifier[] = ['adamfortuna', 'minafi', 'hardcover']
+
+  const finders = projects.map((p) => getTagsByProject(p))
+  const results = await Promise.all(finders)
+
+  const allTags = flatten(results)
+
+  const tagHash = {} as any
+  allTags.forEach((tag) => {
+    if (!tag.count || tag.count === 0) {
+      return
+    }
+
+    if (tagHash[tag.slug]) {
+      tagHash[tag.slug].count += tag.count
+    } else {
+      tagHash[tag.slug] = {
+        count: 0,
+        ...tag,
+      } as Tag
+    }
+  })
+  const unsortedTags: Tag[] = Object.entries(tagHash).map(([_key, val]) => val as Tag)
+  return unsortedTags.sort((a, b) => (a.name.toLocaleUpperCase() > b.name.toLocaleUpperCase() ? 1 : -1))
 }
