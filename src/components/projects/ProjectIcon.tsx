dab0ffb29d04bee9486b1713a9c81472973fc79b
Adam Fortuna <adam@fortuna.name>
2023-01-11 10:55:26 -0700

    Fix typescript issue with next-cloudinary

--- src/components/projects/ProjectIcon.tsx
+++ src/components/projects/ProjectIcon.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/jsx-no-useless-fragment, no-nested-ternary */
-import { useMemo } from 'react'
 import { CldImage } from 'next-cloudinary'
 import { Image } from '@/components/layout/Image'
 
@@ -9,13 +8,6 @@ export interface ProjectIconProps {
 }
 
 export const ProjectIcon = ({ icon_url, size = 64 }: ProjectIconProps) => {
-  const url = useMemo(() => {
-    if (!icon_url) {
-      return ''
-    }
-    return icon_url.replace('https://res.cloudinary.com/dyogenez/image/upload/v1662311966/', '/')
-  }, [icon_url])
-
   if (!icon_url) {
     return <></>
   }
@@ -31,5 +23,5 @@ export const ProjectIcon = ({ icon_url, size = 64 }: ProjectIconProps) => {
       </span>
     )
   }
-  return <CldImage src={url} className="rounded-full bg-white-400" height={size} width={size} alt="Project icon" />
+  return <CldImage src={icon_url} className="rounded-full bg-white-400" height={size} width={size} alt="Project icon" />
 }
