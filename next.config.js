6e03ad136f5dbe19d50aa888d5457c57380a29e9
Adam Fortuna <adam@fortuna.name>
2023-01-11 10:00:31 -0700

    Added bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
   images: {
     loader: 'cloudinary',
     domains: ['res.cloudinary.com'],
@@ -15,4 +19,4 @@ module.exports = {
 
     return config
   },
-}
+})
