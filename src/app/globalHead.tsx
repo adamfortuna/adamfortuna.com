a4e9c0240d653d51ff3f9ccd1b107b86ca886261
Adam Fortuna <adam@fortuna.name>
2023-01-31 19:44:13 -0700

    Switched feed to use Feedburner

--- src/app/globalHead.tsx
+++ src/app/globalHead.tsx
@@ -4,7 +4,7 @@ const GlobalHead = () => {
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link href="/favicon.ico" rel="shortcut icon" />
-      <link rel="alternate" type="application/rss+xml" href={`${process.env.NEXT_PUBLIC_URL}/api/feed`} />
+      <link rel="alternate" type="application/rss+xml" href="https://feeds.feedburner.com/adamfortuna" />
       <link href="https://github.com/adamfortuna" rel="me" />
     </>
   )
