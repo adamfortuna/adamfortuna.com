f9d13af9482f383023a7c79cfc95674e39cbd74a
Adam Fortuna <adam@fortuna.name>
2023-02-01 18:25:03 -0700

    Attempt to precompile tag pagination

--- src/app/blog/tags/[slug]/[page]/page.tsx
+++ src/app/blog/tags/[slug]/[page]/page.tsx
@@ -1,3 +1,22 @@
+import flatten from 'lodash/flatten'
+import range from 'lodash/range'
+import { getTags } from '@/queries/wordpress/getTags'
 import Page from '../page'
 
 export default Page
+
+export async function generateStaticParams() {
+  const tags = (await getTags()).filter((t) => t.count && t.count > 0)
+
+  const pairs = tags.map((tag) => {
+    const PER_PAGE = Number(process.env.NEXT_PUBLIC_ARTICLES_PER_PAGE)
+    const pagesCount = Math.ceil((tag.count || 0) / PER_PAGE)
+
+    return range(1, pagesCount).map((page) => ({
+      slug: tag.slug,
+      page: String(page),
+    }))
+  })
+
+  return flatten(pairs)
+}
