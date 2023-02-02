a7619cdcbf2f6699a4f67a122be050c01bd8303a
Adam Fortuna <adam@fortuna.name>
2023-02-01 20:57:10 -0700

    Pagination for all posts page

--- src/app/blog/all/[page]/page.tsx
+++ src/app/blog/all/[page]/page.tsx
@@ -1,3 +1,18 @@
+import range from 'lodash/range'
+import { getRecentPosts } from '@/queries/wordpress/getRecentPosts'
 import Page from '../page'
 
 export default Page
+
+const PER_PAGE = Number(process.env.NEXT_PUBLIC_ARTICLES_PER_PAGE)
+
+export async function generateStaticParams() {
+  const { totalPages } = await getRecentPosts({
+    count: PER_PAGE,
+    offset: 0,
+  })
+
+  return range(1, totalPages).map((page) => ({
+    page: String(page),
+  }))
+}
