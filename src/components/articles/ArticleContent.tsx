6fab7709acec9a55517257e5da8e99103bcbd1f3
Adam Fortuna <adam@fortuna.name>
2023-02-19 18:48:35 -0700

    Added modula css

--- src/components/articles/ArticleContent.tsx
+++ src/components/articles/ArticleContent.tsx
@@ -5,7 +5,7 @@ import { Article } from '@/types'
 export const ArticleContentHtml = ({ article }: { article: Article }) => {
   return (
     <div
-      className="e-content prose dark:prose-invert mx-auto container"
+      className="e-content prose dark:prose-invert mx-auto"
       dangerouslySetInnerHTML={{ __html: article.content || '' }}
     />
   )
