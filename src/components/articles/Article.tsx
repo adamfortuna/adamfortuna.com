13f260f59ccd4cedc5b8551267cb505697d71d8f
Adam Fortuna <adam@fortuna.name>
2023-11-08 22:38:42 -0700

    Fix layout on single post page

--- src/components/articles/Article.tsx
+++ src/components/articles/Article.tsx
@@ -2,10 +2,8 @@ import { AboutHeader } from '@/components/articles/AboutHeader'
 import { ArticleContentHtml } from '@/components/articles/ArticleContent'
 import { ArticleHeader } from '@/components/articles/ArticleHeader'
 import { ArticleAboutAdamFooter } from '@/components/articles/ArticleAboutAdamFooter'
-import { ReplyViaEmail } from '@/components/articles/ReplyViaEmail'
 import { Comments } from '@/components/comments/Comments'
 import { Page, Post } from '@/types'
-import Cloud from '@/images/cloud.svg'
 
 export interface ArticleHeaderProps {
   article: Page | Post
@@ -22,15 +20,7 @@ export const Article = ({ article }: ArticleHeaderProps) => {
       <ArticleHeader article={article} />
       <ArticleContentHtml article={article} />
       <ArticleAboutAdamFooter />
-      <div className="bg-sky-100 mt-4 md:pt-4 pb-8">
-        <div className="h-16 md:h-24 lg:h-32 overflow-hidden md:-mt-[40px] relative  rotate-180">
-          <Cloud className="w-full xl:bg-cover relative" />
-          <div className="md:hidden bg-white absolute bottom-0 h-8 md:h-2 w-full" />
-        </div>
-
-        <Comments article={article} />
-        <ReplyViaEmail />
-      </div>
+      <Comments article={article} />
     </article>
   )
 }
